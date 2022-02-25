52a31f7ee1a20954434a6a82d17b50c4f3e1ad51
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-02-25 16:37:02 +0500

    feat: added MotorActuator for controlling motor pairs

--- car_app/app/main.cpp
+++ car_app/app/main.cpp
@@ -6,17 +6,30 @@
  *************************************************************************/
 
 #include <QCoreApplication>
+#include <QThread>
 #include <QDebug>
 
-#include <picamera.h>
+#include <motoractuator.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    PICamera camera;
+    MotorActuator leftMotors(1, 4);
+    MotorActuator rightMotors(5, 6);
+    leftMotors.setSpeed(1.0);
+    rightMotors.setSpeed(1.0);
 
-    qDebug() << "Hello from Qt";
+    leftMotors.setDirection(MotorActuator::Direction::Forward);
+    rightMotors.setDirection(MotorActuator::Direction::Forward);
+    QThread::sleep(1);
+
+    leftMotors.setDirection(MotorActuator::Direction::Backward);
+    rightMotors.setDirection(MotorActuator::Direction::Backward);
+    QThread::sleep(1);
+
+    leftMotors.setDirection(MotorActuator::Direction::Still);
+    rightMotors.setDirection(MotorActuator::Direction::Still);
 
     return a.exec();
 }
---
+++ car_app/peripherals/src/motoractuator.cpp
@@ -0,0 +1,68 @@
+/**************************************************************************
+ *
+ *   @author Doniyorbek Tokhirov <tokhirovdoniyor@gmail.com>
+ *   @date 25/02/2022
+ *
+ *************************************************************************/
+
+#include <QDebug>
+
+#include <motoractuator.h>
+
+#include <wiringPi.h>
+#include <softPwm.h>
+
+
+MotorActuator::MotorActuator(int forwardPinN, int backwardPinN)
+{
+    if (wiringPiSetup() < 0) {
+        qDebug() << "Wiring PI was not set up!";
+        return;
+    }
+
+    m_forwardPinN = forwardPinN;
+    m_backwardPinN = backwardPinN;
+
+    pinMode(m_forwardPinN, SOFT_PWM_OUTPUT);
+    pinMode(m_backwardPinN, SOFT_PWM_OUTPUT);
+
+    softPwmCreate(m_forwardPinN, MIN_SPEED, MAX_SPEED);
+    softPwmCreate(m_backwardPinN, MIN_SPEED, MAX_SPEED);
+}
+
+MotorActuator::~MotorActuator()
+{
+}
+
+void MotorActuator::setDirection(Direction direction)
+{
+    m_direction = direction;
+    updateMotors();
+}
+
+void MotorActuator::setSpeed(float speed)
+{
+    m_speed = speed;
+    updateMotors();
+}
+
+void MotorActuator::updateMotors()
+{
+    switch (m_direction) {
+    case Direction::Forward: {
+        softPwmWrite(m_forwardPinN, MAX_SPEED * m_speed);
+        softPwmWrite(m_backwardPinN, MIN_SPEED);
+        break;
+    }
+    case Direction::Backward: {
+        softPwmWrite(m_forwardPinN, MIN_SPEED);
+        softPwmWrite(m_backwardPinN, MAX_SPEED * m_speed);
+        break;
+    }
+    case Direction::Still: {
+        softPwmWrite(m_forwardPinN, MIN_SPEED);
+        softPwmWrite(m_backwardPinN, MIN_SPEED);
+        break;
+    }
+    }
+}
