a0df82983b6e2b2fcc5c8bd5881732e06ce7ce90
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2022-03-14 21:38:52 +0500

    feat: added utility functions for Vector

--- car_app/services/include/vector.h
+++ car_app/services/include/vector.h
@@ -18,6 +18,7 @@ struct Vector
     inline auto length() { return std::sqrt(x * x + y * y); }
     inline auto angle() { return std::atan2(x, y) * 180 / M_PI; }
     inline auto isZero() { return x == 0.f && y == 0.f; }
+    inline auto compare(float x, float y) { return this->x == x && this->y == y; }
 
     void normalize()
     {
@@ -26,9 +27,8 @@ struct Vector
         y /= len;
     }
 
-    bool operator!=(const Vector &other)
-    {
-        return x != other.x || y != other.y;
-    }
+    bool operator==(const Vector &other) { return compare(other.x, other.y); }
+
+    bool operator!=(const Vector &other) { return !(*this == other); }
 };
 
